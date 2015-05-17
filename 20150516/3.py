aa1dd62219ffaa1782bae5040408b5da94af931e
playcrab op <sys@playcrab.com>
2015-05-17 20:06:18 +0800

    05172

--- 20150516/3.py
+++ 20150516/3.py
@@ -1,6 +1,10 @@
 #!/bin/env python
 def operate(input_str):
-    math_l = list(input_str)
+    tmp_l = list(input_str)
+    for i in range(len(tmp_l)):
+        if tmp_l[i] == '+' or tmp_l[i] == '-' or tmp_l[i] == '*' or tmp_l[i] == '/':
+            tmp_l[i] = '_' + tmp_l[i] + '_'
+    math_l = ''.join(tmp_l).split('_')
     while True:
         if ' ' in math_l:
             math_l.remove(' ')
@@ -14,6 +18,7 @@ def operate(input_str):
             math_l[math_l.index('-')-1:math_l.index('-')+2] = [float(math_l[math_l.index('-')-1]) - float(math_l[math_l.index('-')+1])]
         else:
             return math_l[0]
+        print math_l
 while True:
     content = raw_input('input a math expression: ')
     if content:
