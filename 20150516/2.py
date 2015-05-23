cf5e72aa1f70c6f2357cf411c2b5ee7778afefb1
playcrab op <sys@playcrab.com>
2015-05-23 11:24:20 +0800

    0523

--- 20150516/2.py
+++ 20150516/2.py
@@ -1,2 +1,3 @@
 l = [(1,4),(5,1),(2,3)]
 print sorted(l,key=lambda x:max(x))
+print sorted(l,key=lambda (x,y): x * (x > y) + y * (x <= y))
--- 20150516/3.py
+++ 20150516/3.py
@@ -6,9 +6,7 @@ def operate(input_str):
             tmp_l[i] = '_' + tmp_l[i] + '_'
     math_l = ''.join(tmp_l).split('_')
     while True:
-        if ' ' in math_l:
-            math_l.remove(' ')
-        elif '*' in math_l:
+        if '*' in math_l:
             math_l[math_l.index('*')-1:math_l.index('*')+2] = [float(math_l[math_l.index('*')-1]) * float(math_l[math_l.index('*')+1])]
         elif '/' in math_l:
             math_l[math_l.index('/')-1:math_l.index('/')+2] = [float(math_l[math_l.index('/')-1]) / float(math_l[math_l.index('/')+1])]
@@ -18,10 +16,9 @@ def operate(input_str):
             math_l[math_l.index('-')-1:math_l.index('-')+2] = [float(math_l[math_l.index('-')-1]) - float(math_l[math_l.index('-')+1])]
         else:
             return math_l[0]
-        print math_l
 while True:
     content = raw_input('input a math expression: ')
     if content:
-        print 'the sum of your input num: %f' % operate(content)
+        print 'the value of your input num: %f' % operate(content)
     else:
         continue
